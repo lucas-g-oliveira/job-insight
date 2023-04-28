05a1e1cc3332cb1bc8829ea8b8227c23556cb162
Lucas G <lucasg.devmail93@gmail.com>
2023-04-28 10:14:08 -0300

    feat: implemented function get_max_salary

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -1,22 +1,19 @@
 from typing import Union, List, Dict
+import csv
 
 
 def get_max_salary(path: str) -> int:
-    """Get the maximum salary of all jobs
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The maximum salary paid out of all job opportunities
-    """
-    raise NotImplementedError
+    with open(path, "r") as file:
+        data = [*csv.DictReader(file)]
+        sal = [val["max_salary"] for val in data if val["max_salary"] != ""]
+        maxValue = 0
+        for value in sal:
+            try:
+                if int(value) > maxValue:
+                    maxValue = int(value)
+            except ValueError:
+                ''
+        return maxValue
 
 
 def get_min_salary(path: str) -> int:
@@ -64,8 +61,7 @@ def matches_salary_range(job: Dict, salary: Union[int, str]) -> bool:
 
 
 def filter_by_salary_range(
-    jobs: List[dict],
-    salary: Union[str, int]
+    jobs: List[dict], salary: Union[str, int]
 ) -> List[Dict]:
     """Filters a list of jobs by salary range
 
